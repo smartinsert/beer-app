1eea8ceefc7c7aa105f9c18acdd55bb4e9270c0f
Ankit Thakur <ankitccb@gmail.com>
2023-02-07 12:26:53 +0530

    first commit

---
+++ src/main/java/org/acme/Beer.java
@@ -0,0 +1,32 @@
+package org.acme;
+
+import javax.json.bind.annotation.JsonbCreator;
+
+public class Beer {
+    private String name;
+    private String tagline;
+    private double abv;
+
+    public Beer(String name, String tagline, double abv) {
+        this.name = name;
+        this.tagline = tagline;
+        this.abv = abv;
+    }
+
+    @JsonbCreator
+    public static Beer of(String name, String tagline, double abv) {
+        return new Beer(name, tagline, abv);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTagline() {
+        return tagline;
+    }
+
+    public double getAbv() {
+        return abv;
+    }
+}
---
+++ src/main/java/org/acme/BeerResource.java
@@ -0,0 +1,29 @@
+package org.acme;
+
+import io.smallrye.mutiny.Multi;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Path("/beer")
+public class BeerResource {
+
+    @RestClient
+    BeerClient beerClient;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Multi<Beer> beers() {
+        return Multi.createBy().repeating()
+                .supplier(() -> new AtomicInteger(1),
+                        i -> beerClient.getBeers(i.getAndIncrement())
+                ).until(List::isEmpty)
+                .onItem().<Beer>disjoint()
+                .select().where(b -> b.getAbv() > 15.0);
+    }
+}
